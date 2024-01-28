4c2fedcdd46b183b20569720729642d608a8fe8f
Logesh S <83343125+logesh81098@users.noreply.github.com>
2024-01-28 21:04:57 +0530

    Attaching policy to IAM role

--- module/iam-role/main.tf
+++ module/iam-role/main.tf
@@ -19,3 +19,85 @@ resource "aws_iam_role" "s3-to-efs-role" {
 EOF
 }
 
+#############################################################################################################
+#                                         Creating IAM policy
+#############################################################################################################
+resource "aws_iam_policy" "s3-to-efs-policy" {
+  name = "s3-to-efs-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "lambdavpcacces",
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "ec2:CreateNetworkInterface",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DescribeSubnets",
+        "ec2:DeleteNetworkInterface",
+        "ec2:AssignPrivateIpAddresses",
+        "ec2:UnassignPrivateIpAddresses"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "lambdaexecutionpolicy",
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "s3-object-lambda:WriteGetObjectResponse"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "s3bucketlist",
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket",
+        "s3:PutObject",
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::*",
+        "arn:aws:s3:::*/*"
+      ]
+    },
+    {
+      "Sid": "efsaccess",
+      "Effect": "Allow",
+      "Action": [
+        "elasticfilesystem:ClientWrite",
+        "elasticfilesystem:ClientRootAccess",
+        "elasticfilesystem:DescribeFileSystems",
+        "elasticfilesystem:ClientRead",
+        "elasticfilesystem:DescribeMountTargets"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "invokelambda",
+      "Effect": "Allow",
+      "Action": [
+        "lambda:InvokeFunction"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}  
+
+
+#############################################################################################################
+#                                  Attaching Policy with Role
+#############################################################################################################
+resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
+  policy_arn = aws_iam_policy.s3-to-efs-policy.arn
+  role = aws_iam_role.s3-to-efs-role.id
+}
